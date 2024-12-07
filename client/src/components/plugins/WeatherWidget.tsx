40863ad32d19fcb7e6b8cc0ddc6f3d6213f4c8d6
mpduarte <37662379-mpduarte@users.noreply.replit.com>
2024-12-07 02:14:20 +0000

    Agent query: Is the weather widget now displaying weather information instead of a text widget?

--- client/src/components/plugins/WeatherWidget.tsx
+++ client/src/components/plugins/WeatherWidget.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { type PluginComponent } from '../../lib/types';
+import { type PluginComponent, PluginProps } from '../../lib/types';
 import { createPortal } from 'react-dom';
 
 interface WeatherData {
